1d869cf7457528a949d3486aefb0c4018c459f1c
Seenivasan <seenivasanv@mcci.com>
2022-07-11 11:19:01 +0530

    fixed #13 Add token validation for all influx endpoints

--- app/routes/keys.route.js
+++ app/routes/keys.route.js
@@ -24,11 +24,13 @@
 
 const keyctrl = require('../controllers/keys.controller');
 
+const tokenfn = require('../misc/auth');
+
 module.exports = (app) => {
 
-    app.post('/dbs', keyctrl.getdbs);
-    app.post('/meas/:dbn', keyctrl.getmeas);
-    app.post('/fields', keyctrl.getfields);
-    app.post('/tags', keyctrl.gettags);
-    app.post('/tvals/:tkey', keyctrl.gettvals);
+    app.post('/dbs', tokenfn.authenticateJWT, keyctrl.getdbs);
+    app.post('/meas/:dbn', tokenfn.authenticateJWT, keyctrl.getmeas);
+    app.post('/fields', tokenfn.authenticateJWT, keyctrl.getfields);
+    app.post('/tags', tokenfn.authenticateJWT, keyctrl.gettags);
+    app.post('/tvals/:tkey', tokenfn.authenticateJWT, keyctrl.gettvals);
 }
